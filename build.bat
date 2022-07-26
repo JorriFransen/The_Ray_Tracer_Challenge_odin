5ab60ba471739778bc6c9fcc00c6e76f591bbc8d
Jorri Fransen <jorri@jorrifransen.nl>
2022-07-26 14:15:50 +0200

    Very basic build script on windows.

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+mkdir build
+
+set ODINFLAGS=-debug
+
+REM odin build src -out:build/raytracer.exe %ODINFLAGS%
+REM odin build src/putting_it_together_CH01 -out:build/putting_it_together_CH01.exe %ODINFLAGS%
+
+REM odin test tests
+odin build tests --out:build/tests.exe %ODINFLAGS% && build\tests.exe
