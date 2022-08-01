2a1a85f4622473c5e11ed3c9670c7b42a5c776a1
Jorri Fransen <jorri@jorrifransen.nl>
2022-08-01 19:17:07 +0200

    Passing collections in the windows build script.

--- build.bat
+++ build.bat
@@ -3,9 +3,17 @@
 mkdir build
 
 set ODINFLAGS=-debug
+set COLLECTION_FLAGS=-collection:raytracer=src
 
-REM odin build src -out:build/raytracer.exe %ODINFLAGS% && build\raytracer.exe
-REM odin build src/putting_it_together_CH01 -out:build/putting_it_together_CH01.exe %ODINFLAGS%
+set ODINFLAGS=%ODINFLAGS% %COLLECTION_FLAGS%
 
-REM odin test tests
+@echo on
+
+@REM odin build src/putting_it_together -out:build/putting_it_together.exe %odinflags% && build\putting_it_together.exe
+
+@REM odin test tests %odinflags%
+
 odin build tests --out:build/tests.exe %ODINFLAGS% && build\tests.exe
+
+@REM odin build src -out:build/raytracer.exe %ODINFLAGS% && build\raytracer.exe
+
